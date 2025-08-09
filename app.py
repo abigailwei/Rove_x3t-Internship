d481f19c9c688c9cf5e57efae6eafea32863ac35
abigailwei <abigailwei29@gmail.com>
2025-08-08 22:54:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -445,7 +445,7 @@ def main():
         )
         st.components.v1.html(
             f"""
-            <iframe src='{embed_url}'  height='1200' frameborder='0' marginheight='0' marginwidth='0'>
+            <iframe src='{embed_url}'  width = '100%' height='1200' frameborder='0' marginheight='0' marginwidth='0'>
             Loading…
             </iframe>
             """,
@@ -457,6 +457,7 @@ def main():
 if __name__ == "__main__":
 
     main() 
+
 
 
 
